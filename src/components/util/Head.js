2b92ce98bb34fe0a9baeaa1da1c2c3582228c63b
Test <karim_om@me.com>
2018-09-29 19:40:46 +0200

    fixed build

--- src/components/util/Head.js
+++ src/components/util/Head.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-export default class Head extends React.Component() {
-
-
+export default class Head extends React.Component {
   render() {
     return(
       <Helmet>
