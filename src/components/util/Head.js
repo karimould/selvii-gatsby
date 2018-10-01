173f36ca26205d5fb510627bcff13a9e5df79ec9
Test <karim_om@me.com>
2018-10-01 11:57:08 +0200

    Added auto-pop flase option to the cart and trying to fix link on order informations

--- src/components/util/Head.js
+++ src/components/util/Head.js
@@ -9,8 +9,9 @@ export default class Head extends React.Component {
         <meta charSet="utf-8" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
         <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" id="snipcart" 
+        auto-pop="false"
         data-api-key="ZTc4NTUzYmEtYzViMS00ZjViLWE2OTUtYzliOGQzMGYzNDIxNjM2NzI0MzA2OTY1ODUzMTY5"
-        data-cart-custom1-name="Do you accept terms? <a href='/'>Test</a>"
+        data-cart-custom1-name="Mit der Bestellung akzeptieren Sie unsere AGBs? <a href='/'>Test</a>"
         data-cart-custom1-options="true|false"
         data-cart-custom1-required="true"></script>
         <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" type="text/css" rel="stylesheet" />
