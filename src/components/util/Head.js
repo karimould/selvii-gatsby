01cb8ed20d1c9b6b36a77cfaf92060bcb0269d06
Test <karim_om@me.com>
2018-10-01 11:52:08 +0200

    test agbs link in checkout process

--- src/components/util/Head.js
+++ src/components/util/Head.js
@@ -10,7 +10,7 @@ export default class Head extends React.Component {
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
         <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" id="snipcart" 
         data-api-key="ZTc4NTUzYmEtYzViMS00ZjViLWE2OTUtYzliOGQzMGYzNDIxNjM2NzI0MzA2OTY1ODUzMTY5"
-        data-cart-custom1-name="Do you accept terms?"
+        data-cart-custom1-name="Do you accept terms? <a href='/'>Test</a>"
         data-cart-custom1-options="true|false"
         data-cart-custom1-required="true"></script>
         <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" type="text/css" rel="stylesheet" />
