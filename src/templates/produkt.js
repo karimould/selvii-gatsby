2014c1f1eb8fbb92c482fb5133359e889a5ba5a7
Karim Ould Mahieddine <karim_om@me.com>
2019-08-05 19:32:34 +0200

    quick fix

--- src/templates/produkt.js
+++ src/templates/produkt.js
@@ -67,7 +67,7 @@ export const aboutPageQuery = graphql`
           alt
           image {
               childImageSharp {
-                fluid(maxWidth: 500, quality: 100) {
+                fluid(maxWidth: 800, quality: 100) {
                   ...GatsbyImageSharpFluid
                 }
               }
