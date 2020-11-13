fc5ad3ee58938cddaa0075ffe8a708716063b3de
Karim Ould Mahieddine <karim_om@me.com>
2020-11-13 12:05:40 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,77 +1,87 @@
-import React from "react";
-import Wrapper from "../components/wrapper/Wrapper";
-import "../styles/normalize.css";
-import comingSoon from "../img/comingsoon.jpeg";
-import Head from "../components/util/Head";
+import React from 'react'
+import Wrapper from '../components/wrapper/Wrapper'
+import Header from '../components/header/Header'
+import SliderComp from '../components/slider/Slider'
+import Footer from '../components/footer/Footer'
+import '../styles/normalize.css'
+import Head from '../components/util/Head';
+import SocialmediaIndex from '../components/util/socialmedia-index/Socialmedia-index';
 
 export default class IndexPage extends React.Component {
   render() {
+    // const { data } = this.props
+    // const { edges: products } = data.allMarkdownRemark  
+    if (typeof window !== 'undefined') {
+      window.location = '/new';
+    }
+  
     return (
       <Wrapper>
-        <Head title="Selvii Online Shop" desc="Selvii Damenmode online kaufen. Shoppen Sie die neueste Selvii Damenmode." />
+        <Head title="Selvii Online Shop | Die neue Kollektion online kaufen" desc="Selvii Damenmode online kaufen. Shoppen Sie die neueste Selvii Damenmode."/>
         <Header />
         <SliderComp />
         <SocialmediaIndex />
         <Footer />
       </Wrapper>
-    );
+    )
   }
 }
 
- IndexPage.propTypes = {
-   data: PropTypes.shape({
-     allMarkdownRemark: PropTypes.shape({
-       edges: PropTypes.array,
-     }),
-   }),
- }
+// IndexPage.propTypes = {
+//   data: PropTypes.shape({
+//     allMarkdownRemark: PropTypes.shape({
+//       edges: PropTypes.array,
+//     }),
+//   }),
+// }
 
- export const pageQuery = graphql`
-   query IndexQuery {
-     allMarkdownRemark(
-       sort: { order: DESC, fields: [frontmatter___date] },
-       filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
-     ) {
-       edges {
-         node {
-           excerpt(pruneLength: 400)
-           id
-           fields {
-             slug
-           }
-           frontmatter {
-             title
-             templateKey
-             date(formatString: "MMMM DD, YYYY")
-           }
-         }
-       }
-     }
-   }
- `
- export const pageQuery = graphql`
-   query NewProducts {
-     allMarkdownRemark(
-       limit: 6
-       filter: { frontmatter: { new: { eq: true } }}
-     ) {
-       edges {
-         node {
-           fields {
-             slug
-           }
-           frontmatter {
-             available
-             new
-             price
-             title
-             templateKey
-             description
-             featuredImage
-             category_
-           }
-         }
-       }
-     }
-   }
- `
+
+// export const pageQuery = graphql`
+//   query IndexQuery {
+//     allMarkdownRemark(
+//       sort: { order: DESC, fields: [frontmatter___date] },
+//       filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+//     ) {
+//       edges {
+//         node {
+//           excerpt(pruneLength: 400)
+//           id
+//           fields {
+//             slug
+//           }
+//           frontmatter {
+//             title
+//             templateKey
+//             date(formatString: "MMMM DD, YYYY")
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
+// export const pageQuery = graphql`
+//   query NewProducts {
+//     allMarkdownRemark(
+//       limit: 6
+//       filter: { frontmatter: { new: { eq: true } }}
+//     ) {
+//       edges {
+//         node {
+//           fields {
+//             slug
+//           }
+//           frontmatter {
+//             available
+//             new
+//             price
+//             title
+//             templateKey
+//             description
+//             featuredImage
+//             category_
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
