cb4e7d8956b4347e920717e9c76ac7843c40a3b3
Karim Ould Mahieddine <karim_om@me.com>
2020-11-13 09:27:19 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,75 +8,70 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <Wrapper>
-        <Head title="Selvii Online Shop coming soon" desc="Selvii Damenmode online kaufen. Shoppen Sie die neueste Selvii Damenmode." />
-        <div style={{ display: "flex", justifyContent: "center", marginTop: "100px" }}>
-          <img style={{ width: "300px", height: "300px" }} src={comingSoon} alt="selvi coming soon" title="selvi coming soon" />
-        </div>
-        <h1 style={{ textAlign: "center" }}>coming soon</h1>
-        {/* 
+        <Head title="Selvii Online Shop" desc="Selvii Damenmode online kaufen. Shoppen Sie die neueste Selvii Damenmode." />
         <Header />
         <SliderComp />
         <SocialmediaIndex />
-        <Footer /> */}
+        <Footer />
       </Wrapper>
     );
   }
 }
 
-// IndexPage.propTypes = {
-//   data: PropTypes.shape({
-//     allMarkdownRemark: PropTypes.shape({
-//       edges: PropTypes.array,
-//     }),
-//   }),
-// }
+ IndexPage.propTypes = {
+   data: PropTypes.shape({
+     allMarkdownRemark: PropTypes.shape({
+       edges: PropTypes.array,
+     }),
+   }),
+ }
 
-// export const pageQuery = graphql`
-//   query IndexQuery {
-//     allMarkdownRemark(
-//       sort: { order: DESC, fields: [frontmatter___date] },
-//       filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
-//     ) {
-//       edges {
-//         node {
-//           excerpt(pruneLength: 400)
-//           id
-//           fields {
-//             slug
-//           }
-//           frontmatter {
-//             title
-//             templateKey
-//             date(formatString: "MMMM DD, YYYY")
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
-// export const pageQuery = graphql`
-//   query NewProducts {
-//     allMarkdownRemark(
-//       limit: 6
-//       filter: { frontmatter: { new: { eq: true } }}
-//     ) {
-//       edges {
-//         node {
-//           fields {
-//             slug
-//           }
-//           frontmatter {
-//             available
-//             new
-//             price
-//             title
-//             templateKey
-//             description
-//             featuredImage
-//             category_
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+ export const pageQuery = graphql`
+   query IndexQuery {
+     allMarkdownRemark(
+       sort: { order: DESC, fields: [frontmatter___date] },
+       filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+     ) {
+       edges {
+         node {
+           excerpt(pruneLength: 400)
+           id
+           fields {
+             slug
+           }
+           frontmatter {
+             title
+             templateKey
+             date(formatString: "MMMM DD, YYYY")
+           }
+         }
+       }
+     }
+   }
+ `
+ export const pageQuery = graphql`
+   query NewProducts {
+     allMarkdownRemark(
+       limit: 6
+       filter: { frontmatter: { new: { eq: true } }}
+     ) {
+       edges {
+         node {
+           fields {
+             slug
+           }
+           frontmatter {
+             available
+             new
+             price
+             title
+             templateKey
+             description
+             featuredImage
+             category_
+           }
+         }
+       }
+     }
+   }
+ `
